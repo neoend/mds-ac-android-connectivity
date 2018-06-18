1db7ecd3baf67d1ada4daef1e3c4e10c325a65ca
neoend <neo4ngel@gmail.com>
2018-06-18 17:25:06 +0900

    NfcReader : Processing URI payload 2
    modified Activity

--- NFC/NfcReader/app/src/main/java/com/example/hjh/nfcreader/MainActivity.java
+++ NFC/NfcReader/app/src/main/java/com/example/hjh/nfcreader/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hjh.nfcreader;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void processIntent(Intent i) {
+        if ("android.nfc.action.NDEF_DISCOVERED".equals(i.getAction())) {
+            System.out.println("android.nfc.action.NDEF_DISCOVERED");
+        }
         // 관례적으로 NDEF Message는 1개.
         Parcelable[] rawData = i.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
         if (rawData == null) {
@@ -35,13 +39,33 @@ public class MainActivity extends AppCompatActivity {
         }
         // 1st Message
         NdefMessage ndefMsg = (NdefMessage) rawData[0];
-        // Ndef Records
+        // 2. Ndef Records
         NdefRecord[] recArr = ndefMsg.getRecords();
+        NdefRecord record = recArr[0];
 
-        byte[] realData = recArr[0].getPayload();
+        byte[] realData = record.getPayload();
+        String type = new String(record.getType());
+        // U, T, S
+        String strData = null;
+        System.out.println(realData);
+        System.out.println("type: " + type);
         // p.41 참조
-        String strData = new String(realData, 3, realData.length -3);
-        String fullData = new String(realData);
-        infoTv.setText("tag data: " + fullData);
+        switch (type.charAt(0)) {
+            case 'T':
+                strData = new String(realData, 3, realData.length -3);
+                break;
+            case 'U':
+                //strData = new String(realData);
+                Uri uri = record.toUri();
+                strData = uri.toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                break;
+            default:
+                // unknown type
+                break;
+        }
+
+        infoTv.setText("tag data: " + strData);
     }
 }
