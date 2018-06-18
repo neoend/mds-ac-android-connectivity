71a48fac35f1d5c247f2af3c20dd2b91cb65feb7
neoend <neo4ngel@gmail.com>
2018-06-18 16:38:52 +0900

    NfcReader: read nfc payload

--- NFC/NfcReader/app/src/main/java/com/example/hjh/nfcreader/MainActivity.java
+++ NFC/NfcReader/app/src/main/java/com/example/hjh/nfcreader/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.hjh.nfcreader;
 
 import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.tech.Ndef;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -16,7 +21,27 @@ public class MainActivity extends AppCompatActivity {
         infoTv = findViewById(R.id.infoTv);
 
         Intent recI = getIntent();
-        String action = recI.getAction();
-        infoTv.setText("Received action: " + action);
+        processIntent(recI);
+//        String action = recI.getAction();
+//        infoTv.setText("Received action: " + action);
+
+    }
+
+    private void processIntent(Intent i) {
+        // 관례적으로 NDEF Message는 1개.
+        Parcelable[] rawData = i.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawData == null) {
+            return;
+        }
+        // 1st Message
+        NdefMessage ndefMsg = (NdefMessage) rawData[0];
+        // Ndef Records
+        NdefRecord[] recArr = ndefMsg.getRecords();
+
+        byte[] realData = recArr[0].getPayload();
+        // p.41 참조
+        String strData = new String(realData, 3, realData.length -3);
+        String fullData = new String(realData);
+        infoTv.setText("tag data: " + fullData);
     }
 }
