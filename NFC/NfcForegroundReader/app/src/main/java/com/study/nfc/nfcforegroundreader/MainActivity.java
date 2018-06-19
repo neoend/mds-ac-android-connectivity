4b4700ad85f52d00c97e95069df530b889203fa6
neoend <neo4ngel@gmail.com>
2018-06-19 10:37:36 +0900

    NfcForegroundReader : Add uri filter

--- NFC/NfcForegroundReader/app/src/main/java/com/study/nfc/nfcforegroundreader/MainActivity.java
+++ NFC/NfcForegroundReader/app/src/main/java/com/study/nfc/nfcforegroundreader/MainActivity.java
@@ -3,7 +3,11 @@ package com.study.nfc.nfcforegroundreader;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,14 +39,17 @@ public class MainActivity extends AppCompatActivity {
         i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         pIntent = PendingIntent.getActivity(this, 0, i, 0);
         // 태그될때 어떤 태그를 처리할 것인가
-        IntentFilter ndefF = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter filterText = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter filterUri = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter filterUri2 = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
         try {
-            ndefF.addDataType("text/plain");
+            filterText.addDataType("text/plain");
+            filterUri.addDataScheme("http");
+            filterUri2.addDataScheme("https");
         } catch (IntentFilter.MalformedMimeTypeException e) {
             e.printStackTrace();
         }
-
-        filters = new IntentFilter[]{ndefF};
+        filters = new IntentFilter[]{filterText, filterUri, filterUri2};
     }
 
     @Override
@@ -61,6 +68,38 @@ public class MainActivity extends AppCompatActivity {
     private void processIntent(Intent intent) {
         String action = intent.getAction();
         infoTv.setText("action : " + action);
+
+        if ("android.nfc.action.NDEF_DISCOVERED".equals(intent.getAction())) {
+            Parcelable[] rawData = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            // 1st Message ; 일반적으로 메시지는 하나만 사용
+            NdefMessage ndefMsg = (NdefMessage) rawData[0];
+            // 2. Ndef records
+            NdefRecord[] recArr = ndefMsg.getRecords();
+            NdefRecord record = recArr[0];
+
+            byte[] realData = record.getPayload();
+            String type = new String(record.getType());
+            // U, T, S
+            String strData = null;
+            System.out.println(realData);
+            System.out.println("type: " + type);
+            // p.41 참조
+            switch (type.charAt(0)) {
+                case 'T':
+                    strData = new String(realData, 3, realData.length -3);
+                    break;
+                case 'U':
+                    //strData = new String(realData);
+                    Uri uri = record.toUri();
+                    strData = uri.toString();
+                    Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(i);
+                    break;
+                default:
+                    // unknown type
+                    break;
+            }
+        }
     }
 
     @Override
